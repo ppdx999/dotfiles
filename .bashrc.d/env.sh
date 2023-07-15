01c1b1aa7edb0ffdcd145f377aba3f1acf47d6fc
ppdx999 <ppdx999@gmail.com>
2023-07-15 21:06:50 +0900

    [bash] add env

--- .bashrc.d/env.sh
+++ .bashrc.d/env.sh
@@ -1,4 +1,8 @@
 [[ -d "$HOME/.local/bin" ]] && export PATH="$PATH:$HOME/.local/bin"
 [[ -d "$HOME/bin"        ]] && export PATH="$PATH:$HOME/bin"
 
-. "$HOME/.cargo/env"
+[[ -d "$HOME/.cargo/bin" ]] && export PATH="$PATH:$HOME/.cargo/bin"
+[[ -f "$HOME/.cargo/env" ]] && . "$HOME/.cargo/env"
+
+# ghcup-env
+[ -f "/home/fujis/.ghcup/env" ] && . "/home/fujis/.ghcup/env"
