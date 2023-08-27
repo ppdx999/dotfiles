b02d9ae5a12ff22a61b140a9004668b8fd6c051a
ppdx999 <ppdx999@gmail.com>
2023-08-27 14:46:02 +0900

    [bash] add env for pnpm

--- .bashrc.d/env.sh
+++ .bashrc.d/env.sh
@@ -12,3 +12,9 @@ if [[ -d "$HOME/.deno" ]]; then
   export DENO_INSTALL="$HOME/.deno"
   export PATH="$DENO_INSTALL/bin:$PATH"
 fi
+
+# pnpm
+if [[ -d "$HOME/.local/share/pnpm" ]]; then
+  export PNPM_HOME="$HOME/.local/share/pnpm"
+  export PATH="$PNPM_HOME:$PATH"
+fi
