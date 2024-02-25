2bc7f3f5c6bc8506e28c9918de92613290f45dee
ppdx999 <ppdx999@gmail.com>
2024-02-25 11:12:11 +0900

    create install/docker.sh

---
+++ install/docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ue
+
+if ! command -v docker &> /dev/null ; then
+  # Add Docker's official GPG key:
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
+  # Insall the Docker packages:
+  sudo -y apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+# Add $USER to docker group
+getent group docker &> /dev/null || sudo groupadd docker # create docker group if not exist
+sudo usermod -aG docker $USER
