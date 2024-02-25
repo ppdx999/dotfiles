8a5b057c09013b7457198a760367ead565e9eaf1
ppdx999 <ppdx999@gmail.com>
2024-02-25 21:23:00 +0900

    add test

--- Makefile
+++ Makefile
@@ -4,8 +4,12 @@ install:
 
 .PHONY: build
 build:
-	docker compose build
+	docker compose build --no-cache
 
 .PHONY: run
 run:
 	docker compose run --rm main
+
+.PHONY: test
+test:
+	docker compose run --rm main /dotfiles/test.sh
---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ue
+
+error_exit () {
+  printf 'Error: %s\n' "$1" >&2
+  exit 1
+}
+
+assert_command_exist() {
+  for cmd in "$@"; do
+   command -v $cmd &> /dev/null || error_exit "Command $cmd not found"
+  done
+}
+
+assert_symlink_exist() {
+  for f in "$@"; do
+    [[ -L $f ]] || error_exit "Symlink $f not found"
+  done
+}
+
+assert_symlink_exist $HOME/.tmux.conf $HOME/.config/nvim $HOME/.config/lvim
