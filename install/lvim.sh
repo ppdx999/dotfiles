6c22ead3c206a498075df5739be32ffe03357584
ppdx999 <ppdx999@gmail.com>
2024-02-26 19:49:47 +0900

    fixup cargo path resolve error

--- install/lvim.sh
+++ install/lvim.sh
@@ -2,5 +2,5 @@
 set -eu
 
 if [ ! -f "$HOME/.local/bin/lvim" ]; then
-  LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh | sed -e 's;cargo install;$HOME/.cargo/bin/cargo install;' ) -- -y
+  LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh | sed -e 's;cargo install;$HOME/.cargo/bin/cargo install;' -e 's;-v cargo;-v $HOME/.cargo/bin/cargo;' ) -- -y
 fi
