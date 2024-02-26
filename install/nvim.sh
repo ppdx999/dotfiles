8de462ab1d7f613c8e9740a544b8c17826b30b64
ppdx999 <ppdx999@gmail.com>
2024-02-26 15:02:42 +0900

    improve robustness

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -eu
 
+sudo rm -rf /usr/bin/nvim
+sudo rm -rf /usr/lib/nvim
+sudo rm -rf /usr/share/nvim
+
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar -zxvf nvim-linux64.tar.gz
-mv nvim-linux64/bin/nvim /usr/bin/nvim
-mv nvim-linux64/lib/nvim /usr/lib/nvim
-mv nvim-linux64/share/nvim/ /usr/share/nvim
+sudo mv nvim-linux64/bin/nvim /usr/bin/nvim
+sudo mv nvim-linux64/lib/nvim /usr/lib/nvim
+sudo mv nvim-linux64/share/nvim/ /usr/share/nvim
 rm -rf nvim-linux64
 rm nvim-linux64.tar.gz
