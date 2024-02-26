8280226a4924bc0b1306e8475252fabef132c715
ppdx999 <ppdx999@gmail.com>
2024-02-26 11:51:54 +0900

    setup PAHT for go and rust

---
+++ .config/bash/go.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [[ -d "$HOME/go/bin" ]]; then
+  export PATH="$HOME/go/bin:$PATH"
+fi
---
+++ .config/bash/rust.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -f "$HOME/.cargo/env" ]]; then
+  source "$HOME/.cargo/env"
+fi
+
+if [[ -d "$HOME/.cargo/bin" ]]; then
+    export PATH="$HOME/.cargo/bin:$PATH"
+fi
