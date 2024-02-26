d2147e6a1f2c636a7bf7dc3f119f795556399631
ppdx999 <ppdx999@gmail.com>
2024-02-26 14:38:30 +0900

    setup nodejs when fnm is installed

--- .config/bash/fnm.sh
+++ .config/bash/fnm.sh
@@ -2,4 +2,8 @@
 
 if [[ -f "$HOME/.cargo/bin/fnm" ]]; then
   eval "$($HOME/.cargo/bin/fnm env --use-on-cd)"
+
+  if [[ $($HOME/.cargo/bin/fnm current) = "none" ]]; then
+    $HOME/.cargo/bin/fnm install --lts
+  fi
 fi
--- install/fnm.sh
+++ install/fnm.sh
@@ -2,3 +2,11 @@
 set -eu
 
 $HOME/.cargo/bin/cargo install fnm
+
+if [[ -f "$HOME/.cargo/bin/fnm" ]]; then
+  eval "$($HOME/.cargo/bin/fnm env --use-on-cd)"
+
+  if [[ $($HOME/.cargo/bin/fnm current) = "none" ]]; then
+    $HOME/.cargo/bin/fnm install --lts
+  fi
+fi
