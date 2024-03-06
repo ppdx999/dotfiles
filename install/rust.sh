eb52e95fa7a24fc95d93a609b45a6a23c21aeffe
ppdx999 <ppdx999@gmail.com>
2024-03-07 00:38:01 +0900

    improve idempotency

--- install/rust.sh
+++ install/rust.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -ue
 
+if [[ -f "$HOME/.cargo/env" ]]; then
+  echo "Rust is already installed"
+  exit 0
+fi
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
