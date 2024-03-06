2a3a4374fa9ebd6084164e72c073f94b433297b2
ppdx999 <ppdx999@gmail.com>
2024-03-06 19:57:11 +0900

    [pyenv] install libs to build python

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -eu
 
+
+sudo apt install -y \
+  build-essential \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  curl \
+  libncursesw5-dev \
+  xz-utils \
+  tk-dev \
+  libxml2-dev \
+  libxmlsec1-dev \
+  libffi-dev \
+  liblzma-dev
+
 if [ ! -d "$HOME/.pyenv" ]; then
   curl https://pyenv.run | bash
 fi
