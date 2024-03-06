f4c8ac296b37391506166ab327bee9e6a88daaf3
ppdx999 <ppdx999@gmail.com>
2024-03-07 00:36:49 +0900

    add pyenv deps

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -eu
 
+# install python dependencies
+sudo apt install -y build-essential gdb lcov pkg-config \
+    libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
+    libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
+    lzma lzma-dev tk-dev uuid-dev zlib1g-dev
 
+# install pyenv dependencies
 sudo apt install -y \
   build-essential \
   libssl-dev \
