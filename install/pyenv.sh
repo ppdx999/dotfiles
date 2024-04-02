28ddaf1815bc2add759ecbe682c71eb4223df0d8
ppdx999 <ppdx999@gmail.com>
2024-04-02 23:02:18 +0900

    fixup build error

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 set -eu
 
-# install python dependencies
-sudo apt install -y build-essential gdb lcov pkg-config \
-    libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
-    libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
-    lzma lzma-dev tk-dev uuid-dev zlib1g-dev
-
-# install pyenv dependencies
-sudo apt install -y \
-  build-essential \
-  libssl-dev \
-  zlib1g-dev \
-  libbz2-dev \
-  libreadline-dev \
-  libsqlite3-dev \
-  curl \
-  libncursesw5-dev \
-  xz-utils \
-  tk-dev \
-  libxml2-dev \
-  libxmlsec1-dev \
-  libffi-dev \
-  liblzma-dev
-
 if [ ! -d "$HOME/.pyenv" ]; then
   curl https://pyenv.run | bash
 fi
