4cd78501e1ccbb2ff6788e93a0b7ab23182c2a68
fujis <ppdx999@gmail.com>
2025-07-27 22:26:21 +0900

    fixup neovim install url

--- install/nvim.sh
+++ install/nvim.sh
@@ -5,10 +5,10 @@ sudo rm -rf /usr/bin/nvim
 sudo rm -rf /usr/lib/nvim
 sudo rm -rf /usr/share/nvim
 
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-tar -zxvf nvim-linux64.tar.gz
-sudo mv nvim-linux64/bin/nvim /usr/bin/nvim
-sudo mv nvim-linux64/lib/nvim /usr/lib/nvim
-sudo mv nvim-linux64/share/nvim/ /usr/share/nvim
-rm -rf nvim-linux64
-rm nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
+tar -zxvf nvim-linux-x86_64.tar.gz
+sudo mv nvim-linux-x86_64/bin/nvim /usr/bin/nvim
+sudo mv nvim-linux-x86_64/lib/nvim /usr/lib/nvim
+sudo mv nvim-linux-x86_64/share/nvim/ /usr/share/nvim
+rm -rf nvim-linux-x86_64
+rm nvim-linux-x86_64.tar.gz
