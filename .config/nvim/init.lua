cf7889f6974b9b6bc6c5e04f2afacf0cb3c6b375
fujis <ppdx999@gmail.com>
2025-12-03 17:26:13 +0900

    vimの設定いろいろと追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -443,6 +443,34 @@ require("lazy").setup({
 		'weirongxu/plantuml-previewer.vim'
 	},
   -- /------------------------------
+  -- | Claude
+  -- -------------------------------/
+	{
+		"coder/claudecode.nvim",
+		dependencies = { "folke/snacks.nvim" },
+		config = true,
+		keys = {
+			{ "<leader>a", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+			-- { "<leader>a", nil, desc = "AI/Claude Code" },
+			-- { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+			-- { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+			-- { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+			-- { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+			-- { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+			-- { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+			-- { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+			-- {
+			-- 	"<leader>as",
+			-- 	"<cmd>ClaudeCodeTreeAdd<cr>",
+			-- 	desc = "Add file",
+			-- 	ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+			-- },
+			-- -- Diff management
+			-- { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+			-- { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+		},
+	},
+  -- /------------------------------
   -- | MISC
   -- -------------------------------/
 	{
@@ -479,12 +507,33 @@ require("lazy").setup({
 	{
 		'iamcco/markdown-preview.nvim',
 	},
+	-- /------------------------------
+	-- | UI Enhancements
+	-- -------------------------------/
 	{
 		'b0o/incline.nvim',
 		config = function()
 			require('incline').setup()
 		end
 	},
+	{
+		'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+	-- /------------------------------
+	-- | which-key
+	-- -------------------------------/
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
