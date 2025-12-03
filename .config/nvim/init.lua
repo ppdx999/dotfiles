17b1d9fffaa831502eebe7b1d5ed8f780db29cee
fujis <ppdx999@gmail.com>
2025-12-03 17:58:45 +0900

    ターミナルの起動をリーダ経由に

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -487,7 +487,7 @@ require("lazy").setup({
 		'akinsho/toggleterm.nvim', version = "*",
 		config = function()
 			require("toggleterm").setup({
-				open_mapping = [[<c-\>]],
+				open_mapping = [[<leader>\]],
 				direction = 'vertical',
 				size = 60,
 			})
