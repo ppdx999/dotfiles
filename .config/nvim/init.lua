e127e29dfcc6d968b523301a4720e27e549bf492
fujis <ppdx999@gmail.com>
2025-12-03 17:36:47 +0900

    barbarを<leader>-でトグル

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -521,13 +521,13 @@ require("lazy").setup({
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
     init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
+    opts = {},
+    keys = {
+      { '<leader>-', function()
+        vim.o.showtabline = vim.o.showtabline == 0 and 2 or 0
+      end, desc = 'Toggle tabline' },
     },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    version = '^1.0.0',
   },
 	-- /------------------------------
 	-- | which-key
